17ee974314e227dea9debf723079a58da397bf73
Betsinat Wendwesen <Betsinat39@gmail.com>
2026-04-03 18:14:05 +0300

    Time: 163 ms (16.63%), Space: 22.5 MB (67.42%) - LeetHub

---
+++ 0475-heaters/0475-heaters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        def closest_distance(heaters: List[int], house: int) -> int:
+            left, right = 0, len(heaters) - 1
+            min_dist = float('inf')
+            
+            while left <= right: 
+                mid = (left + right) // 2
+                min_dist = min(min_dist, abs(heaters[mid] - house))
+                
+                if heaters[mid] < house:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return min_dist
+        
+        heaters.sort()
+        radius = 0
+        
+        for house in houses:
+            radius = max(radius, closest_distance(heaters, house))
+        
+        return radius
