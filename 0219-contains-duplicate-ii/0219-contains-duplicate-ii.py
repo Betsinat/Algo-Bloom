7f926603edf5554903c4483ca58330c610b299ba
Betsinat Wendwesen <Betsinat39@gmail.com>
2026-07-02 10:06:14 +0300

    Time: 28 ms (77.1%), Space: 36.7 MB (29.77%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -3,7 +3,7 @@ class Solution:
        seen = {}
        for i in range(len(nums)):
              if nums[i] in seen:
-                if  abs(i - seen[nums[i]]) <= k:
+                if i - seen[nums[i]] <= k:
                     return True
              seen[nums[i]] = i
        return False
